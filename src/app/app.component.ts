3ad853510bdc4497f8b7a5bcaa74a51a36a4c9f1
Lachlan Heywood <lachlan@heywood.net.au>
2020-10-27 17:23:35 -0400

    add repro steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import type { OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnChanges {
   title = 'type-only-test';
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
 }
